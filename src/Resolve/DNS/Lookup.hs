c97055d68427308c8daab20a73513d8acaa9e471
Zesen Qian <git@riaqn.org>
2017-01-01 13:11:18 +0800

    EDNS assumed finished

--- src/Resolve/DNS/Lookup.hs
+++ src/Resolve/DNS/Lookup.hs
@@ -7,6 +7,7 @@ import qualified Resolve.DNS.Types as DNS
 import Resolve.DNS.EDNS.Types as EDNS
 import qualified Resolve.DNS.EDNS.Encode as EE
 
+
 import qualified Resolve.DNS.Transport.UDP as UDP
 
 import Control.Monad.STM
@@ -21,16 +22,15 @@ data Query = Query { qquestion :: [Question]
                    , qopt :: [Option]
                    }
              
-data Response = Response { ranswer ::  [RR],
-                           rauthority :: [RR],
-                           radditional :: [RR],
-                           ropt :: Maybe OPT,
-                           raa :: Bool
+data Response = Response { ranswer ::  [RR]
+                         , rauthority :: [RR]
+                         , radditional :: [RR]
+                         , ropt :: Maybe [Option]
                          }
 
-data Error = Truncated (Maybe OPT)
+data Error = Truncated
            | WierdResponse String
-           | ErrorResponse RCODE
+           | ErrorResponse (Maybe Word8) RCODE
              deriving (Typeable, Show)
 
 instance Exception Error where
@@ -51,77 +51,76 @@ new :: Config -> IO (T.Resolver Query Response)
 new c = do
   p <- newTVarIO 512 -- server's UDP payload
   return $ T.Resolver { T.resolve = resolve $ Lookup { config = c
-                                                 , p_that = p
-                                                 }
+                                                     , p_that = p
+                                                     }
                       , T.delete = return ()
                     }
 
 resolve :: Lookup -> T.Resolve Query Response
 resolve l q = do
-  this <- p_this $ config $ l
-  that <- atomically $ readTVar (p_that l)
-  setPayload (config l) (if this < that then this else that)
-  
   m <- tryJust (\e -> if (fromException e == Just UDP.QueryTooLong) then Just Nothing
                       else case fromException e of
-                             Just (Truncated opt') -> Just $ opt'
+                             Just Truncated -> Just Nothing
                              _ -> Nothing)
-       (resolve' (udp $ config $ l) this q)
-  let setup o = atomically $ when (udp_size o >= 512) $ writeTVar (p_that l) $ udp_size o
-  let setup' b = case ropt b of
-        Nothing -> return ()
-        Just o -> setup o
-  
+       (resolve' (udp $ config $ l) l q)
+
   b' <- case m of
-    Left b -> case b of
-      Nothing -> return Nothing
-      Just o -> setup o >> return Nothing
-    Right b -> (setup' b) >> (return $ Just b)
+    Left b -> return Nothing
+    Right b -> (return $ Just b)
+    
   case b' of
       Just b -> return b
-      Nothing -> do
-        b' <- resolve' (tcp $ config $ l) this q
-        setup' b' 
-        return b'
+      Nothing -> resolve' (tcp $ config $ l) l q
   
-resolve' :: T.Resolve Message Message -> Word16 -> T.Resolve Query Response
-resolve' r p_this q = do
+resolve' :: T.Resolve Message Message -> Lookup -> T.Resolve Query Response
+resolve' r l q = do
+  this <- p_this $ config $ l
+  that <- atomically $ readTVar (p_that l)
+  
+  setPayload (config l) (if this < that then this else that)
+  
   b <- r $ Message { header = Header { ident = 0
-                                   , qr = DNS.Query
-                                   , opcode = STD
-                                   , aa = False
-                                   , tc = False
-                                   , rd = True
-                                   , ra = False
-                                   , zero = 0
-                                   , rcode = NoErr
-                                   }
-                 , question = (qquestion q)
-                 , answer = []
-                 , authority = []
-                 , additional = []
-                 , opt = Just $ OPT { udp_size = p_this
-                                    , ext_rcode = 0
-                                    , version = 0
-                                    , dnssec_ok = False
-                                    , options = (qopt q)
-                                    }
-                 }
+                                     , qr = DNS.Query
+                                     , opcode = STD
+                                     , aa = False
+                                     , tc = False
+                                     , rd = True
+                                     , ra = False
+                                     , zero = 0
+                                     , rcode = NoErr
+                                     }
+                   , question = (qquestion q)
+                   , answer = []
+                   , authority = []
+                   , additional = []
+                   , opt = Just $ OPT { udp_size = this
+                                      , ext_rcode = 0
+                                      , version = 0
+                                      , dnssec_ok = False
+                                      , options = (qopt q)
+                                      }
+                   }
   let h = header b
   when (ident h /= 0 || 
         qr h /= DNS.Query ||
         opcode h /= STD ||
         rd h /= True
        ) $ throwIO $ WierdResponse "some field is strange"
-  when (rcode h /= NoErr) $ throwIO $ ErrorResponse $ rcode h
-  when (tc $ h ) $ throwIO $ Truncated $ opt b
+
+  case opt b of
+    Nothing -> return ()
+    Just o -> do
+      atomically $ when (udp_size o >= 512) $ writeTVar (p_that l) $ udp_size o
+      when ((version o /= 0) || (dnssec_ok o /= False)) $ throwIO $ WierdResponse "some EDNS field is strange"
+    
+  when ((maybe False (\o -> ext_rcode o /=0 ) $ opt  b) || rcode h /= NoErr) $ throwIO $ ErrorResponse  (ext_rcode <$> opt b) (rcode h)
+  when (tc $ h ) $ throwIO $ Truncated
   let  xs = qquestion q
        ys = question b
   when (not $ null (xs \\ ys) && null (ys \\ xs)) $ throwIO $ WierdResponse "question section not match"
 
   return $ Response { ranswer = answer b
-                  , rauthority = answer b
-                  , radditional = additional b
-                  , ropt = opt b
-                  , raa = aa h
-                  }
+                    , rauthority = answer b
+                    , radditional = additional b
+                    , ropt = options <$> opt b
+                    }
